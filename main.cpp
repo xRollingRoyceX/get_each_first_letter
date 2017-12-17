a433995496174000940c92955bfc6d11601ab294
Sean <jesky.sean@outlook.com>
2017-12-16 19:06:20 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,19 +4,21 @@
  */
 #include <std_lib_facilities.h>
 //get first char in each word.
-string get_char(string::size_type cnt, string s, string holder)
+const string get_char(string::size_type cnt, string &s,string holder)
 {
+    
     for (cnt = 0; cnt != s.size(); ++cnt) 
     {
-        if (isalpha(s[cnt - 1]) != isalpha(s[cnt]) &&
-                !ispunct(s[cnt - 1] && !ispunct(s[cnt]))) 
+        
+        if (isalpha(s[cnt - 1]) != isalpha(s[cnt]) && !ispunct(s[cnt])) 
         {
             holder += s[cnt];
         }
     }
     return holder;
 }
-int main() {
+int main()
+{
     /*  Work on figuring out how to fix minor, 
      *  errors like '[rogram' and 'h.ow'.  
      */
@@ -26,7 +28,22 @@ int main() {
             " to debug for";
     string hold;
     cout << get_char(stct, s, hold) << endl;
-    //current result: 
-    // .. 
-    //E Y O S H t i a t-t f m l.r t s h.o i w a w t d f
-}
+} 
+//Current Result: 
+// .....>
+//      E Y O S H t i a t-t f m l.r t s h.o i w a w t d f
+//After Debugging Issue, Result:) 
+// .....>
+//      E Y O S H t i a tt f m lr t s ho i w a w t d f
+//  quite happy i solved this problem after a small step away from the computer..
+//  my error was stupid but worth learning about and seeing how and why.
+/*
+ * I was explaining numbers and letters on computers to my dad, with a simple example
+ * c++ teaches you so much. delete this and line above w/ multi-line comments
+    
+    char c;
+    for(int i = 0; i != 123; ++i){
+        cout << ++c;
+    }      
+ */
+   
